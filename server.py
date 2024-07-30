8f90064cfe9f09726f7bad3de6b1c5e18e3817ed
slegro97 <sam.legro97@gmail.com>
2024-07-30 19:25:23 -0400

    Website updates, adding personal info

--- server.py
+++ server.py
@@ -4,6 +4,15 @@ import csv
 app = Flask(__name__)
 
 
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route("/")
 def my_home():
     return render_template('index.html')
@@ -12,23 +21,6 @@ def my_home():
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
-
-
-def write_to_file(data):
-    with open('database.txt', 'a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        txt_writer = database.write(f'Email: {email}\nSubject: {subject}\nMessage: {message}\n\n')
-
-
-def write_to_csv(data):
-    with open('database.csv', 'a', newline='') as database2:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email, subject, message])
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
